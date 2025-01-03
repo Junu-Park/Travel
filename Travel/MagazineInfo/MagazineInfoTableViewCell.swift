f1d1d9f49bb7e8cd27fe59522268455f8e12c810
Park Junwoo <ays99111@naver.com>
2025-01-03 22:52:22 +0900

    [feat] MagazineInfo Tab 구현

---
+++ Travel/MagazineInfo/MagazineInfoTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MagazineInfoTableViewCell.swift
+//  Travel
+//
+//  Created by 박준우 on 1/3/25.
+//
+
+import UIKit
+
+class MagazineInfoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var infoView: UIView!
+    
+    @IBOutlet weak var infoImage: UIImageView!
+    
+    @IBOutlet weak var infoTitle: UILabel!
+    
+    @IBOutlet weak var infoSubTitle: UILabel!
+    
+    @IBOutlet weak var infoDate: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- Travel/MagazineInfo/MagazineInfoTableViewController.swift
+++ Travel/MagazineInfo/MagazineInfoTableViewController.swift
@@ -6,84 +6,48 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MagazineInfoTableViewController: UITableViewController {
 
+    var info = MagazineInfo().magazine
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        navigationItem.title = "SeSAC TRAVEL"
+        tableView.separatorStyle = .none
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return info.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineInfoCell", for: indexPath) as! MagazineInfoTableViewCell
+        
+        cell.infoImage.contentMode = .scaleToFill
+        cell.infoImage.layer.cornerRadius = 10
+        cell.infoImage.clipsToBounds = true
+        cell.infoImage.kf.setImage(with: URL(string: info[indexPath.row].photo_image), placeholder: UIImage(systemName: "photo"))
+        
+        cell.infoTitle.numberOfLines = 0
+        cell.infoTitle.font = .boldSystemFont(ofSize: 20)
+        cell.infoTitle.text = info[indexPath.row].title
+        
+        cell.infoSubTitle.numberOfLines = 0
+        cell.infoSubTitle.font = .boldSystemFont(ofSize: 14)
+        cell.infoSubTitle.text = info[indexPath.row].subtitle
+        cell.infoSubTitle.textColor = .lightGray
 
-        // Configure the cell...
-
+        cell.infoDate.numberOfLines = 0
+        cell.infoDate.font = .boldSystemFont(ofSize: 14)
+        cell.infoDate.text = info[indexPath.row].date.getDateFromyyMMddString()
+        cell.infoDate.textColor = .lightGray
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        self.tableView.frame.height / 2
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
